108073cc6fbb0ba4f9dca16f92e23edc556311ea
kopas2010 <nekomaneki135km@gmail.com>
2022-06-24 17:05:14 +0000

    update sessions&registrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :candidates
-  devise_for :voters
-  devise_for :admins
+  devise_for :candidates, skip: [:passwords], controllers: {
+  registrations: "candidate/registrations",
+  sessions: 'candidate/sessions'
+}
+
+  devise_for :voters, skip: [:passwords], controllers: {
+  registrations: "voter/registrations",
+  sessions: 'voter/sessions'
+}
+  devise_for :admins, skip: [:registrations, :passwords], controllers: {
+  sessions: "admin/sessions"
+}
   get 'homes/top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
